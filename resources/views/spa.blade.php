c3727e18e2509ad7c955b4a5be509146fa7d7081
jc <jctech0412@gmail.com>
2023-10-20 05:07:35 -0400

    upt

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -8,23 +8,21 @@ $config = [
 $appJs = mix('dist/js/app.js');
 $appCss = mix('dist/css/app.css');
 @endphp
+
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <title>{{ config('app.name') }}</title>
-
-  <link rel="stylesheet" href="{{ (str_starts_with($appCss, '//') ? 'http:' : '').$appCss }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ (str_starts_with($appCss, '//') ? 'http:' : '').$appCss }}">
 </head>
+
 <body>
-  <div id="app"></div>
-
-  <script>
-    window.config = @json($config);
-  </script>
-
-  <script src="{{ (str_starts_with($appJs, '//') ? 'http:' : '').$appJs }}"></script>
+    <div id="app"></div>
+    <script>
+        window.config = @json($config);
+    </script>
+    <script src="{{ (str_starts_with($appJs, '//') ? 'http:' : '').$appJs }}"></script>
 </body>
 </html>
