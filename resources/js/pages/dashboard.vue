3984ad380f575448e5d471956dc01220e51c107f
jc <jctech0412@gmail.com>
2023-10-20 03:30:48 -0400

    upt

---
+++ resources/js/pages/dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    layout: 'basic',
+
+    metaInfo () {
+        return { title: this.$t('home') }
+    },
+
+    data: () => ({
+        title: window.config.appName
+    }),
+
+    computed: mapGetters({
+        authenticated: 'auth/check'
+    })
+}
+</script>
