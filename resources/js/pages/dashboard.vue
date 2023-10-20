3b73393e0f0ef7dcff10bdf89d8b94c90a05d9cb
jc <jctech0412@gmail.com>
2023-10-20 05:00:16 -0400

    u

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,25 +1,16 @@
 <template>
     <div>
-        
+        {{ message }}
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-    layout: 'basic',
-
-    metaInfo () {
-        return { title: this.$t('home') }
-    },
 
     data: () => ({
-        title: window.config.appName
-    }),
-
-    computed: mapGetters({
-        authenticated: 'auth/check'
+        title: window.config.appName,
+        message: "test"
     })
 }
 </script>
